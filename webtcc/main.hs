1a15139fbc64879d122dee1fad0a27334aee1543
danpochini <pocchini@live.dk>
2016-05-15 20:23:27 +0000

    Pagina Serviços

--- webtcc/main.hs
+++ webtcc/main.hs
@@ -18,13 +18,124 @@ staticFiles "static"
 mkYesod "SauipeExpress" [parseRoutes|
 / HomeR GET 
 /quemsomos QuemSomosR GET
+/servicos ServicosR GET 
 /static StaticR Static getStatic
---/servicos ServicosR GET 
 --/contato ContatoR GET 
 |]
 
+
+getServicosR :: Handler Html
+getServicosR = defaultLayout $ do
+        setTitle "Sauípe Express|Serviços"
+        addStylesheet $ StaticR css_bootstrap_css
+        addStylesheet $ StaticR css_fontawesomemin_css
+        addStylesheet $ StaticR css_main_css
+        addStylesheet $ StaticR css_principal_css
+        addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"
+        addScriptRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+        toWidgetHead 
+                [hamlet|
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <link rel="icon" href=@{StaticR imagens_icones_iconeCoqueiroFundo_png} type="image/x-icon">
+                |]
+        toWidget 
+                [whamlet|
+                    <nav class="navbar navbar-default navbar-static-top menu cor1"> 
+                        <div class="container">
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">
+                                    <span class="sr-only">Navegação
+                                    <span class="icon-bar">
+                                    <span class="icon-bar">
+                                    <span class="icon-bar">
+                                <a class="navbar-brand" href=@{HomeR}>Sauípe Express
+                            <div class="collapse navbar-collapse" id="navbar-ex-collapse">
+                                <ul class="nav navbar-nav navbar-right">
+                                    <li>
+                                        <a href=@{HomeR}>Home
+                                    <li>
+                                        <a href=@{QuemSomosR}>Quem Somos
+                                    <li>
+                                        <a href=@{ServicosR}>Serviços
+                                    <li>
+                                        <a href="../view/contato.html">Contato
+                    <div class="section">
+                        <header class="container">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <img src=@{StaticR imagens_logotipo10anos_png} class="img-responsive logotipo">
+                                    <p>Frete e entregas para todo o Brasil.
+                                <div class="links">
+                                    <p>
+                                        <img src=@{StaticR imagens_icones_icone_tel_png} alt="Telefone para contato" title="Telefone para contato Sauípe Express"> Telefone:(13)3223-9211 ou 3224-5876
+                                    <p>
+                                        <img src=@{StaticR imagens_icones_icone_whatsapp_png} alt="Celular para contato" title="Celular para contato Sauípe Express"> Celular:(13)99747-7862
+                                    <p>
+                                        <img src=@{StaticR imagens_icones_icone_cel_png} alt="ID nextel" title="ID Nextel Sauípe Express"> Nextel(ID):129*20237
+                                    <p>
+                                        <a href="https://www.facebook.com/armando.barros.5661?fref=pb&amp;hc_location=profile_browser" title="página do facebook Sauípe Express"><img src=@{StaticR imagens_icones_icone_facebook_png} alt="página do facebook"> Curta nossa página no facebook
+                                    <p>
+                                        <a href="../view/contato.html"><img src=@{StaticR imagens_icones_icone_email_png} alt="email para contato" title="Email para contato Sauípe Express"> contato@sauipeexpress.com.br
+                    <div class="section">
+                        <div class="container fundo1">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <img class="hidden-xs lado1" src=@{StaticR imagens_motoLogo1_png}>
+                                    <h1>Serviços
+                                    <p>Ao contratar os nossos serviços você agilizará suas entregas e retiradas
+                                        de malotes, duplicatas, notas fiscais, serviços, bancários, pequenos e
+                                        médios volumes entre outros, com total rapidez e segurança, pois contamos
+                                        com uma equipe especializada para cada tipo de serviço, personalizando
+                                        o atendimento para a necessidade de cada cliente, em diversos segmentos
+                                        como:
+                                    <p>Indústrias, bancos, financeiras, consultórios, concessionárias, contabilidades,
+                                        advocacias, farmácias, restaurantes, deliverys , e todos estabelescimentos
+                                        ou empresas que buscam uma solução definitiva e econômica em transportes
+                                        urgentes.
+                                    <h2>Mensal
+                                    <p>Colocamos à disposição de sua empresa, um ou mais Motofretistas, uniformizados
+                                        e equipados com CELULAR ou RADIOCOMUNICAÇÃO que servirão a sua empresa
+                                        em qualquer tipo de serviço externo nos horários determinados em contrato.
+                                    <h3>Integral
+                                    <p>Segunda à sexta feira jornada de 8h/dia.
+                                    <img class="hidden-xs lado2" src=@{StaticR imagens_montanaLogo1_png}>
+                                    <h3>Meio Período(Manhã)
+                                    <p>Jornada de 4h/dia Obs.: Em caso de algum imprevisto a substituição do
+                                        condutor ou veículo será efetuada no maximo em 90 minutos.
+                                    <h2>Esporádico
+                                    <p>Serviços de entregas rápidas de encomendas leves de um ponto para o outro
+                                        da cidade, este serviço atende a pessoas físicas e jurídicas, com rapidez
+                                        e qualidade. O valor do serviço é definido previamente de acordo com a
+                                        tabela de localidades, em função da distância.
+                                    <p>
+                                        <a href="../view/contato.html">Entre em contato conosco e veja os detalhes deste tipo de serviço
+                    <div class="section">
+                        <footer class="container">
+                            <div class="vcard row">
+                                <div class="col-md-6">
+                                    <h2 class="fn">Sauípe Express Transportes Rápidos Ltda.
+                                    <h3>Mapa do Site
+                                    <a href=@{HomeR}>Home|
+                                    <a href=@{QuemSomosR}>Quem Somos|
+                                    <a href="../view/servicos.html">Serviços|
+                                    <a href="../view/contato.html">Contato
+                                <div class="adr">Endereço:
+                                    <br>
+                                    <span class="street-addresss">Av. Afonso Pena, 45 - Macuco,
+                                    <br>
+                                    <span class="locality">Santos-
+                                    <span class="region">SP
+                                    <br>
+                                    <span title="Celular Sauípe Express" class="tel">Celular: (13)99747-7862
+                                    <br>
+                                    <span class="tel" title="Telefone Sauípe Express">Telefone: (13)3223-9211 ou 3224-5876 <br>Nextel(ID):129*20237
+                                    <br>
+                                <a href="../view/contato.html" class="email" alt="Email Sauípe Express" title="Link para página de contato"> contato@sauipeexpress.com.br
+            |]
+
 getQuemSomosR :: Handler Html
-getQuemSomosR = defaultLayout $ do 
+getQuemSomosR = defaultLayout $ do
         setTitle "Sauípe Express|QuemSomos"
         addStylesheet $ StaticR css_bootstrap_css
         addStylesheet $ StaticR css_fontawesomemin_css
