1000dc04d1f50807fe45254840dfbcbe1f221b2e
danpochini <pocchini@live.dk>
2016-05-16 00:11:12 +0000

    Pagina Checar Cadastro vindo do BD

--- webtcc/main.hs
+++ webtcc/main.hs
@@ -73,17 +73,6 @@ postUsuarioR = do
                FormSuccess prod -> (runDB $ insert prod) >>= \piid -> redirect (ChecarR piid)
                _ -> redirect ErroR
 
-
-getChecarR :: UsuariosId -> Handler Html
-getChecarR pid = do
-    usu <- runDB $ get404 pid
-    defaultLayout [whamlet|
-        <p><b> #{usuariosNome  usu}
-        <p><b> #{usuariosLogin usu}  
-        <p><b> #{usuariosSenha usu}
-    |]
-    
-
 -- se o usuario nao exisitr, dá erro 404
 getActionR :: UsuariosId -> Handler ()
 getActionR pid = do
@@ -103,6 +92,7 @@ deleteActionR pid = do
     runDB $ delete pid
     sendResponse (object [pack "resp" .= pack "DELETADO"])
 
+-- mensagem de erro caso não consiga cadastrar
 getErroR :: Handler Html
 getErroR = defaultLayout [whamlet|
     <p>não foi possivel cadastrar, tente novamente
@@ -111,8 +101,6 @@ getErroR = defaultLayout [whamlet|
 instance RenderMessage SauipeExpress FormMessage where
     renderMessage _ _ = defaultFormMessage
     
---data Usuario = Usuario deriving Show
-
 type Form a = Html -> MForm Handler (FormResult a, Widget)
 
 usuarioForm :: Form Usuarios 
@@ -222,8 +210,105 @@ getUsuarioR = do
                                 <a href=@{ContatoR} class="email" alt="Email Sauípe Express" title="Link para página de contato"> contato@sauipeexpress.com.br                        
         |]
         
-                  
-          
+------------------------------------------------------------------
+
+getChecarR :: UsuariosId -> Handler Html
+getChecarR pid = do
+    usu <- runDB $ get404 pid
+    defaultLayout $ do 
+        setTitle "Sauípe Express|Checar Cadastro"
+        addStylesheet $ StaticR css_bootstrap_css
+        addStylesheet $ StaticR css_fontawesomemin_css
+        addStylesheet $ StaticR css_main_css
+        addStylesheet $ StaticR css_principal_css
+        addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"
+        addScriptRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+        toWidget [cassius|
+                #user h1
+                   text-align: center;
+                   font-weight: bold;
+                   font: 30px "typewriter", sans-serif;
+                #user p
+                   text-align: center;
+                   font: 20px "typewriter", sans-serif;
+        |]
+        toWidgetHead [hamlet|
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <link rel="icon" href=@{StaticR imagens_icones_iconeCoqueiroFundo_png} type="image/x-icon">
+        |]
+        [whamlet| 
+                    <nav class="navbar navbar-default navbar-static-top menu cor1"> 
+                        <div class="container">
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">
+                                    <span class="sr-only">Navegação
+                                    <span class="icon-bar">
+                                    <span class="icon-bar">
+                                    <span class="icon-bar">
+                                <a class="navbar-brand" href=@{HomeR}>Sauípe Express
+                            <div class="collapse navbar-collapse" id="navbar-ex-collapse">
+                                <ul class="nav navbar-nav navbar-right">
+                                    <li>
+                                        <a href=@{HomeR}>Home
+                                    <li>
+                                        <a href=@{QuemSomosR}>Quem Somos
+                                    <li>
+                                        <a href=@{ServicosR}>Serviços
+                                    <li>
+                                        <a href=@{ContatoR}>Contato
+                    <div class="section">
+                        <header class="container">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <img src=@{StaticR imagens_logotipo10anos_png} class="img-responsive logotipo">
+                                    <p>Frete e entregas para todo o Brasil.
+                                <div class="links">
+                                    <p>
+                                        <img src=@{StaticR imagens_icones_icone_tel_png} alt="Telefone para contato" title="Telefone para contato Sauípe Express"> Telefone:(13)3223-9211 ou 3224-5876
+                                    <p>
+                                        <img src=@{StaticR imagens_icones_icone_whatsapp_png} alt="Celular para contato" title="Celular para contato Sauípe Express"> Celular:(13)99747-7862
+                                    <p>
+                                        <img src=@{StaticR imagens_icones_icone_cel_png} alt="ID nextel" title="ID Nextel Sauípe Express"> Nextel(ID):129*20237
+                                    <p>
+                                        <a href="https://www.facebook.com/armando.barros.5661?fref=pb&amp;hc_location=profile_browser" title="página do facebook Sauípe Express"><img src=@{StaticR imagens_icones_icone_facebook_png} alt="página do facebook"> Curta nossa página no facebook
+                                    <p>
+                                        <a href=@{ContatoR}><img src=@{StaticR imagens_icones_icone_email_png} alt="email para contato" title="Email para contato Sauípe Express"> contato@sauipeexpress.com.br
+                    <div class="section">
+                        <div class="container fundo1">
+                            <div class="row">
+                                <div class="col-md-12" id="user">
+                                            <h1> Checar Usuário Cadastrado 
+                                            <br>
+                                            <p>Nome:  #{usuariosNome  usu}
+                                            <p>Login: #{usuariosLogin usu}  
+                                            <p>Senha: #{usuariosSenha usu}
+                                            <br>
+                    <div class="section">
+                        <footer class="container">
+                            <div class="vcard row">
+                                <div class="col-md-6">
+                                    <h2 class="fn">Sauípe Express Transportes Rápidos Ltda.
+                                    <h3>Mapa do Site
+                                    <a href=@{HomeR}>Home|
+                                    <a href=@{QuemSomosR}>Quem Somos|
+                                    <a href=@{ServicosR}>Serviços|
+                                    <a href=@{ContatoR}>Contato
+                                <div class="adr">Endereço:
+                                    <br>
+                                    <span class="street-addresss">Av. Afonso Pena, 45 - Macuco,
+                                    <br>
+                                    <span class="locality">Santos-
+                                    <span class="region">SP
+                                    <br>
+                                    <span title="Celular Sauípe Express" class="tel">Celular: (13)99747-7862
+                                    <br>
+                                    <span class="tel" title="Telefone Sauípe Express">Telefone: (13)3223-9211 ou 3224-5876 <br>Nextel(ID):129*20237
+                                    <br>
+                                <a href=@{ContatoR} class="email" alt="Email Sauípe Express" title="Link para página de contato"> contato@sauipeexpress.com.br                        
+        |]
+
+              
   
 ------------------------------------------------------------------
 
